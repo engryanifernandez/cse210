31968b7b2173fa997d6086d54c311cdeb68a4d64
engryanifernandez <sisterfernandez.byupathway@gmail.com>
2024-11-10 00:36:29 +0800

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "";
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+            int guess = 0;
+            int attempt = 0;
+
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempt++;
+
+                if (number > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guess it!");
+                    Console.WriteLine($"You made {attempt} guesses.");
+                    Console.Write("Do you want to play again? (yes/no)" );
+                    playAgain = Console.ReadLine()?.ToLower();
+                }
+            } 
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
